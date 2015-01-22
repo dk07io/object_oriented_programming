052d68b567a7d8cee5249f3f1dd45b8024ec616c
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-22 17:47:46 -0500

    initial start

--- marsrover.rb
+++ marsrover.rb
@@ -1,9 +1,42 @@
 #marsrover.rb
+rover1= (1,2,N)
 
-class Rover
+class Plateau
 	def initialize(x,y,direction)
 		@x=x
 		@y=y
-		@direciton=direction
+		@direction=direction
 	end
+
+	def initial_position
+
+	end
+end
+
+
+
+
+class Rover
+	attr 
+	def initialize(x,y,direction)
+		@x=x
+		@y=y
+		@direction=direction
+	end
+
+	def read_instruction(l,r,m)
+		
+
+
+	def move()
+	end
+
+	def turn()
+	end
+
+	end
+
+def 
+
+
 end	
