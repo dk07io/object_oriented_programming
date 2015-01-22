82f5f16646a14937d66ae2dbfc80ad3052404d83
Daniel Kim <dk07io@Daniels-MacBook-Pro-3.local>
2015-01-22 14:47:26 -0500

    Called the teach methond on instructor and learn method on Student.

--- people.rb
+++ people.rb
@@ -1,5 +1,6 @@
 # people.rb
 
+#class Person that is parented to Student and Instructor to share name and greeting methods
 class Person
 	def initialize(name)
 		@name=name
@@ -10,20 +11,37 @@ class Person
 	end
 end
 
+#class for student that is parented to Person
 class Student<Person
 	def learn
-		puts "I get it!"
+		"I get it!"
 	end
 end
 
+#class for Instructor that is parented to Person
 class Instructor<Person
 	def teach
 		"Everything in Ruby is an Object"
 	end
 end
 
+#creating instance for instructor Chris 
 instructor=Instructor.new("Chris")
+#call out Chris's greeting
 puts instructor.greeting
+#call the teach method on instructor instance
+puts instructor.teach
 
+
+#creating instance for student Cristina
 student=Student.new("Cristina")
+#call out Cristina's greeting
 puts student.greeting
+#call out teach method on student isntance
+puts student.learn
+#call the teach method on your student instance
+puts student.teach
+# it returns an error because teach method is not definied in class Student!
+
+
+
