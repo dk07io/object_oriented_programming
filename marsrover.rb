1f6c68fb86907b55f7c7fb270f0176059dad9e81
Daniel Kim <dk07io@Daniels-MacBook-Pro.local>
2015-01-31 17:35:25 -0500

    Made a method to get instruction based on the each character of the input string

--- marsrover.rb
+++ marsrover.rb
@@ -2,45 +2,69 @@
 # rover1= (1,2,N)
 
 # class Plateau
-# 	def initialize(x,y,direction,input)
-# 		@x=x
-# 		@y=y
-# 		@direction=direction
-# 		@input=input
-# 	end
+#   def initialize(x,y,direction,input)
+#     @x=x
+#     @y=y
+#     @direction=direction
+#     @input=input
+#   end
 
-# 	def initial_position
+#   def initial_position
 
-# 	end
+#   end
 # end
 
 class Rover
-	def initialize(x,y,direction,code)
-		@x=x
-		@y=y
-		@direction=direction
-		@code=code
-	end
+  attr_accessor :x, :y, :direction, :string
+  
+  def initialize(x = 0, y = 0, direction, string)
+    @x=x.to_i
+    @y=y.to_i
+    @direction=direction
+    @string = string
+  end
 
-	def read_instruction(code)
-		code.each_char do |character|
-			if character=="L"
-				turn
-			elsif character=="M"
-				move
-			end
-	end
+  def read_instruction
+    initial_position = @x, @y
+    puts initial_position
+    string.each_char do |string_character|
+      
+      if string_character == "L"
+        puts "L"
+        # move
+      elsif
+        string_character == "R"
+        puts "R"
+      else
+        string_character == "M"
+        puts "M"
+      
+      end
+    end
+  end
 
-	def move
-		(x,y+1)
-		"#{@x}+1 , {y}"
+# def move
+#     case @direction 
+#     when "N"
 
-	end
+#     if 
 
-	def turn
-	end
+#   end
+# end
+  
+#   def move
+#     case @direction
+#     when "N"
+#       @y+=1
+#     when "S"
+#       @y-=1
+#     when "W"
+#       @x-=1
+#     when "E"
+#       @x-=1
+#     end
+#   end
+# end
 
-end
-
-
-#rover1=Rover.new()
+rover_one = Rover.new(1,2,"N","LMLMLMLM")
+rover_one.read_instruction
